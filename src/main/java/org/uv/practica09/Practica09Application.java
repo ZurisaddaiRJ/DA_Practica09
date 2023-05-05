a3bb6ccb931ddc1a09fb2c16e89f6ac676070a25
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-05-05 11:08:15 -0500

    Correcion de paquete 4

---
+++ src/main/java/org/uv/practica09/Practica09Application.java
@@ -0,0 +1,19 @@
+package org.uv.practica09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+
+//jkjkjkjkjkjkj
+
+@SpringBootApplication
+public class Practica09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica09Application.class, args);
+	}
+
+}
